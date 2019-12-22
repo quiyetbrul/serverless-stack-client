6cddabf322f281e0b97095719f1995105d8a41df
Quiyet Brul <qbrul0514@gmail.com>
2019-12-22 15:44:07 -0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "qb-notes-app-upload"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://rag340pn0g.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Ycid1zhLj",
+      APP_CLIENT_ID: "dvu4j23fn2jdj0gqsmupbb6nb",
+      IDENTITY_POOL_ID: "us-east-1:7d854431-a425-4b93-9fdd-7fb7fcdd929a"
+    }
+  };
